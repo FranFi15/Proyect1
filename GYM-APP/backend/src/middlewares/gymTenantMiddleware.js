8633b5fe60a3239f7685b1f69d8b4564579558a7
FranFi15 <102774445+FranFi15@users.noreply.github.com>
2025-07-21 10:42:35 -0300

    api/paass

--- GYM-APP/backend/src/middlewares/gymTenantMiddleware.js
+++ GYM-APP/backend/src/middlewares/gymTenantMiddleware.js
@@ -1,7 +1,18 @@
 // gym-app-backend/src/middlewares/gymTenantMiddleware.js
 import connectToGymDB from '../config/mongoConnectionManager.js'; 
 
+const PUBLIC_PATHS = [
+    '/api/users/forgot-password',
+    '/api/users/reset-password', 
+    '/api/users/handle-reset-link' 
+];
+
 const gymTenantMiddleware = async (req, res, next) => {
+const isPublicPath = PUBLIC_PATHS.some(path => req.path.startsWith(path));
+    if (isPublicPath) {
+        return next();
+    }
+
     const clientId = req.headers['x-client-id'];
 
     if (!clientId) {
