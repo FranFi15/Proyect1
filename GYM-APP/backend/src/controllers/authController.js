16aae235fdccb887c2520620fa5467e42566ed7f
FranFi15 <102774445+FranFi15@users.noreply.github.com>
2025-07-24 23:48:12 -0300

    register admin

--- GYM-APP/backend/src/controllers/authController.js
+++ GYM-APP/backend/src/controllers/authController.js
@@ -18,11 +18,37 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error('Ya existe un usuario con este email.');
     }
     
-    const userData = { nombre, apellido, email, contraseña, dni, fechaNacimiento: new Date(fechaNacimiento), telefonoEmergencia, direccion: direccion || '', numeroTelefono: numeroTelefono || '', obraSocial: obraSocial || '', roles: ['cliente'], sexo: sexo || 'Otro' };
+    // --- LÓGICA MODIFICADA ---
+    // 1. Contamos cuántos usuarios existen en la base de datos de este gimnasio.
+    const userCount = await User.countDocuments();
+
+    // 2. Si no hay usuarios (count es 0), el rol será 'admin'. De lo contrario, será 'cliente'.
+    const roles = userCount === 0 ? ['admin'] : ['cliente'];
+    
+    const userData = { 
+        nombre, 
+        apellido, 
+        email, 
+        contraseña, 
+        dni, 
+        fechaNacimiento: new Date(fechaNacimiento), 
+        telefonoEmergencia, 
+        direccion: direccion || '', 
+        numeroTelefono: numeroTelefono || '', 
+        obraSocial: obraSocial || '', 
+        roles: roles, // 3. Asignamos el rol determinado dinámicamente.
+        sexo: sexo || 'Otro' 
+    };
+    
     const user = await User.create(userData);
 
     if (user) {
-        res.status(201).json({_id: user._id, nombre: user.nombre, email: user.email, token: generateToken(user._id, user.roles, user.email, user.nombre) });
+        res.status(201).json({
+            _id: user._id, 
+            nombre: user.nombre, 
+            email: user.email, 
+            token: generateToken(user._id, user.roles, user.email, user.nombre) 
+        });
     } else {
         res.status(400);
         throw new Error('Datos de usuario inválidos.');
