21fce54ff6c6ec55e8fd215a980690bc95f9a190
FranFi15 <102774445+FranFi15@users.noreply.github.com>
2025-09-19 17:07:32 -0300

    mp dale port favor

--- GYM-APP/backend/src/controllers/mpConnectController.js
+++ GYM-APP/backend/src/controllers/mpConnectController.js
@@ -5,34 +5,39 @@ import getModels from '../utils/getModels.js';
 const generateConnectUrl = asyncHandler(async (req, res) => {
     const { platform } = req.body;
     const redirectUri = `${process.env.SERVER_URL}/api/connect/mercadopago/callback`;
-    
-    const state = `${req.gymId}|${platform}`;
-    
+    const state = platform;
     const authUrl = `https://auth.mercadopago.com.ar/authorization?client_id=${process.env.MP_APP_ID}&response_type=code&platform_id=mp&state=${state}&redirect_uri=${redirectUri}`;
     res.json({ authUrl });
 });
 
 const handleConnectCallback = asyncHandler(async (req, res) => {
     const { code, state } = req.query;
-    if (!code || !state) throw new Error('Respuesta inválida de Mercado Pago.');
+    const platform = state;
+
+    if (!code || !platform) throw new Error('Respuesta inválida de Mercado Pago.');
     
-    const [clientId, platform] = state.split('|');
-    if (!clientId || !platform) throw new Error('El parámetro de estado tiene un formato inválido.');
-    
-    const gymDBConnection = await getDbConnectionByClientId(clientId);
-    const { Settings } = getModels(gymDBConnection);
+    const { Settings } = getModels(req.gymDBConnection);
     
     const client = new MercadoPagoConfig({ accessToken: process.env.MP_SECRET_KEY, options: { clientId: process.env.MP_APP_ID }});
     const oauth = new OAuth(client);
 
-    const response = await oauth.create({ });
-    
+    const response = await oauth.create({
+        body: {
+            client_secret: process.env.MP_SECRET_KEY,
+            client_id: process.env.MP_APP_ID,
+            code,
+            redirect_uri: `${process.env.SERVER_URL}/api/connect/mercadopago/callback`,
+        }
+    });
+
+    const { access_token, refresh_token, user_id } = response;
+
     await Settings.findByIdAndUpdate('main_settings', {
-        mpAccessToken: response.access_token,
-        mpRefreshToken: response.refresh_token,
-        mpUserId: response.user_id,
+        mpAccessToken: access_token,
+        mpRefreshToken: refresh_token,
+        mpUserId: user_id,
         mpConnected: true,
-    }, { upsert: true, new: true });
+    }, { upsert: true });
 
     const baseUrl = platform === 'mobile' ? process.env.ADMIN_PANEL_URL_MOBILE : process.env.ADMIN_PANEL_URL_WEB;
     const finalRedirectUrl = `${baseUrl}/class-type?mp-status=success`;
--- GYM-APP/backend/src/models/Settings.js
+++ GYM-APP/backend/src/models/Settings.js
@@ -1,8 +1,5 @@
 import mongoose from 'mongoose';
 
-// Ya no necesitamos bcrypt aquí
-// import bcrypt from 'bcryptjs'; 
-
 const settingsSchema = new mongoose.Schema({
     _id: {
         type: String,
