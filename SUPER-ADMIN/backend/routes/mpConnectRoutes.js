7d7189f5d323a4a39e56a12d97ac6980554b4cb4
FranFi15 <102774445+FranFi15@users.noreply.github.com>
2025-09-22 18:18:11 -0300

    mp

--- SUPER-ADMIN/backend/routes/mpConnectRoutes.js
+++ SUPER-ADMIN/backend/routes/mpConnectRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { generateConnectUrl, handleConnectCallback } from '../controllers/mpConnectController.js';
-import { protect } from '../middlewares/authMiddleware.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
