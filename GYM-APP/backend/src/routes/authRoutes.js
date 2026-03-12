2ad8e515c33aabab67202b0c91c8788ba74cb787
FranFi15 <102774445+FranFi15@users.noreply.github.com>
2026-03-12 16:09:24 -0300

    lalala iii

--- GYM-APP/backend/src/routes/authRoutes.js
+++ GYM-APP/backend/src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerUser, loginUser } from '../controllers/authController.js';
+import { registerUser, loginUser, refreshToken } from '../controllers/authController.js';
 import gymTenantMiddleware from '../middlewares/gymTenantMiddleware.js';
 import { protect } from '../middlewares/authMiddleware.js';
 
