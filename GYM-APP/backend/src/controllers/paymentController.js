6e22a7340f38005aa7a10f1805fb4d675cc1fba4
FranFi15 <102774445+FranFi15@users.noreply.github.com>
2026-04-14 20:05:55 -0300

    routes

--- GYM-APP/backend/src/controllers/paymentController.js
+++ GYM-APP/backend/src/controllers/paymentController.js
@@ -229,6 +229,8 @@ const processTransferTicket = asyncHandler(async (req, res) => {
 export {
     createPackage,
     getPackages,
+    updatePackage,
+    deletePackage,
     submitTransferReceipt,
     getPendingRequests,
     processTransferTicket
